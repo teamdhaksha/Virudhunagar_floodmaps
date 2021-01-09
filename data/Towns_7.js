9c6d4ac99947ed091303feffee24f0ab2c11ae41
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-01-09 13:49:35 +0530

    Add files via upload

---
+++ data/Towns_7.js
@@ -0,0 +1,13 @@
+var json_Towns_7 = {
+"type": "FeatureCollection",
+"name": "Towns_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Rajapalayam" }, "geometry": { "type": "Point", "coordinates": [ 77.557469294108913, 9.454282072698948, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Sivakasi" }, "geometry": { "type": "Point", "coordinates": [ 77.799669821158332, 9.453039524290428, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Srivilliputtur" }, "geometry": { "type": "Point", "coordinates": [ 77.630142916836377, 9.514474596056715, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Virudhunagar" }, "geometry": { "type": "Point", "coordinates": [ 77.962805902727695, 9.586703705654815, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Aruppukottai" }, "geometry": { "type": "Point", "coordinates": [ 78.098230518602918, 9.509403507577439, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Sattur" }, "geometry": { "type": "Point", "coordinates": [ 77.915828512819914, 9.363319547429874, -999999.0 ] } }
+]
+}
